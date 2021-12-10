51c29b2949ede37c673a2777586d653b0d177244
VikaIT <75212958+VikaIT@users.noreply.github.com>
2021-12-11 01:59:38 +0300

    Update MainWindow.xaml.cs

--- CodeNames/Players/MainWindow.xaml.cs
+++ CodeNames/Players/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +20,43 @@ namespace Players
     /// </summary>
     public partial class MainWindow : Window
     {
+        private const int size = 5;
+        private Button[,] buttons = new Button[size, size];
         public MainWindow()
         {
             InitializeComponent();
+            AddingButtons();
+            /* Button button1 = new Button();
+             button1.Background = new SolidColorBrush(Colors.Azure);
+             button1.Content = "Progtime";
+             button1.Foreground = new SolidColorBrush(Colors.White);
+             grid1.Children.Add(button1);
+             //grid1.Children.Add(new Button());
+             // назначить элемент на позицию грида
+             Grid.SetColumn(button1, 1);
+             Grid.SetRow(button1, 1);
+             // подписка на событие Click
+             // Метод = обработчик события
+             button1.Click += Button1_Click;*/
+        }
+        public void AddingButtons()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Button b = new Button();
+                    Grid.SetColumn(b, j);
+                    Grid.SetRow(b, i);
+                    buttons[i, j] = b;
+                    buttons[i, j].Click += Button1_Click;
+                    grid.Children.Add(b);
+                }
+            }
+        }
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
